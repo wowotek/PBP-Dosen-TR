17c0f897eac81fd284d039278461c490a786333a
bimoverkill <bimoverkill@gmail.com>
2019-11-26 15:25:47 +0700

    Add some view page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="free-educational-responsive-web-template-webEdu">
+	<meta name="author" content="webThemez.com">
+	<title>Blaemt Senior Highschool</title>
+	<link rel="favicon" href="{{asset('frontend/image/favicon.png')}}">
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700" media="screen">
+	<link rel="stylesheet" href="{{asset('frontend/css/bootstrap.min.css')}}">
+	<link rel="stylesheet" href="{{asset('frontend/css/font-awesome.min.css')}}"> 
+	<link rel="stylesheet" href="{{asset('frontend/css/bootstrap-theme.css')}}" media="screen"> 
+  <link rel="stylesheet" href="{{asset('frontend/css/style.css')}}">
+  <link rel='stylesheet' id="camera-css"  href="{{asset('frontend/css/camera.css')}}" type="text/css" media="all"> 
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="assets/js/html5shiv.js"></script>
+	<script src="assets/js/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+	<!-- Fixed navbar -->
+	<div class="navbar navbar-inverse">
+		<div class="container">
+			<div class="navbar-header">
+				<!-- Button for smallest screens -->
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+			    <a class="navbar-brand" href="index.html">
+                    <img src="{{asset('frontend/image/logo-hor.png')}}" alt="Logo">
+                </a>
+        </div>
+        <div class="navbar-collapse collapse">
+            <ul class="nav navbar-nav pull-left mainNav">
+                <li><a href="{{url('home')}}">Beranda</a></li>
+                <li class="active"><a href="{{url('#')}}">Tentang</a></li>
+                <li><a href="#">Daftar PPDB</a></li>
+            </ul>
+            <ul class="nav navbar-nav pull-right mainNav">
+                <li><a href="#">Login</a></li>
+            </ul>
+            </div>
+            <!--/.nav-collapse -->
+        </div>
+    </div>
+  <!-- /.navbar -->
+
+  <div class="container">
+    <div class="row">
+        <div class="col">
+            <div class="grey-box-icon">
+                <div class="icon-box-top grey-box-icon-pos">
+                    <img src="{{asset('frontend/image/1visi.png')}}" alt="" />
+                </div><!--icon box top -->
+                <h3>Menjadi Pelajar yang Berpendidikan, Berjiwa Santuy, dan Berhobi Rebahan.</h3>
+            </div>
+        </div><!--/span3-->
+        <div class="col">
+            <div class="grey-box-icon">
+                <div class="icon-box-top grey-box-icon-pos">
+                    <img src="{{asset('frontend/image/1misi.png')}}" alt="" />
+                </div><!--icon box top -->
+                <h4>1. Menjadikan pelajar yang berpendidikan baik dan berintelektual tinggi dalam kehidupan masyarakat.</h4>
+                <h4>2. Meningkatkan jiwa santuy siswa di kehidupan sehari-hari guna menghindari ketegangan hidup.</h4>
+                <h4>3. Berperan aktif serta ikut memiliki hobi rebahan di setiap harinya untuk mengistirahatkan badan dan pikiran.</h4>
+            </div>
+        </div><!--/span3-->
+        <div class="col">
+            <div class="grey-box-icon">
+                <div class="icon-box-top grey-box-icon-pos">
+                    <img src="{{asset('frontend/image/1sejarah.png')}}" alt="" />
+                </div><!--icon box top -->
+                <h4>
+                    Blaemt Senior High School didirikan pada tanggal 20 November 1850. Sekolah ini didirikan dengan naungan Yayasan Santoryuu no Amaterasu.
+                    Pada Tahun 1865, banyaknya kelas bertambah menjadi 10 kelas. Dalam rangka meningkatkan kemampuan akademik dibangun laboratorium fisika, kimia, biologi, dan bahasa. Pada Tahun 1900 Blaemt Senior High School memperoleh sertifikat akrditasi pada Agustus 1900.
+                </h4>
+                <h5>Pimpinan : </h5>
+                <h5>- Senju Hashirama (1950 - 1955)</h5>
+                <h5>- Senju Tobirama (1955 - 1980)</h5>
+                <h5>- Sarutobi Hiruzen (1980 - 2000)</h5>
+                <h5>- Namikaze Minato (1990 - 1992)</h5>
+                <h5>- Senju Tsunade (2000 - 2005)</h5>
+                <h5>- Hatake Kakashi (2005 - 2011)</h5>
+                <h5>- Uzumaki Naruto (2011 - Sekarang)</h5>
+            </div>
+        </div><!--/span3-->
+    </div>
+  </div>
+    
+  <footer id="footer">
+	<div class="container">
+    <div class="row">
+        <div class="footerbottom">
+        <div class="col-md-3 col-sm-6">
+            <div class="footerwidget">
+            <a class="navbar-brand" href="#">
+                <img src="{{asset('frontend/image/logo-hor.png')}}" alt="Logo">
+            </a>
+            </div>
+        </div>
+    <div class="col-md-3 col-sm-6">
+        &nbsp;
+    </div>
+    <div class="col-md-3 col-sm-6">
+        &nbsp;
+    </div>
+    <div class="col-md-3 col-sm-6">
+        &nbsp;
+    </div>
+    <div class="col-md-3 col-sm-6">
+        &nbsp;
+    </div>
+    <div class="col-md-3 col-sm-6">
+        &nbsp;
+    </div>
+    <div class="col-md-3 col-sm-6"> 
+      <div class="footerwidget"> 
+        <h4>Contact</h4> 
+                <p></p>
+            <div class="contact-info"> 
+                <i class="fa fa-map-marker"></i> &nbsp; Jl. Mantareno No. 13<br>
+                <i class="fa fa-phone"></i> &nbsp; +62 666 619 123 <br>
+                <i class="fa fa-envelope-o"></i> &nbsp; contact@blaemt.com
+          </div> 
+          </div><!-- end widget --> 
+        </div>
+      </div>
+    </div>
+			<div class="social text-center">
+				<a href="#"><i class="fa fa-twitter"></i></a>
+				<a href="#"><i class="fa fa-instagram"></i></a>
+				<a href="#"><i class="fa fa-github"></i></a>
+			</div>
+
+			<div class="clear"></div>
+			<!--CLEAR FLOATS-->
+		</div>
+		<div class="footer2">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 panel">
+						<div class="panel-body">
+							<p class="text-right">
+								Copyright &copy; 2014.
+							</p>
+						</div>
+					</div>
+
+				</div>
+				<!-- /row of panels -->
+			</div>
+		</div>
+	</footer>
+
+	<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+	<script src="{{asset('frontend/js/modernizr-latest.js')}}"></script> 
+	<script type="text/javascript" src="{{asset('frontend/js/jquery.min.js')}}"></script>
+  <script type="text/javascript" src="{{asset('frontend/js/fancybox/jquery.fancybox.pack.js')}}"></script>
+  <script type="text/javascript" src="{{asset('frontend/js/jquery.mobile.customized.min.js')}}"></script>
+  <script type="text/javascript" src="{{asset('frontend/js/jquery.easing.1.3.js')}}"></script> 
+  <script type="text/javascript" src="{{asset('frontend/js/camera.min.js')}}"></script>
+  <script src="{{asset('frontend/js/bootstrap.min.js')}}"></script>
+	<script src="{{asset('frontend/js/custom.js')}}"></script>
+    <script>
+		jQuery(function(){
+			
+			jQuery('#camera_wrap_4').camera({
+                transPeriod: 500,
+                time: 3000,
+				height: '600',
+				loader: 'false',
+				pagination: true,
+				thumbnails: false,
+				hover: false,
+                playPause: false,
+                navigation: false,
+				opacityOnGrid: false,
+				imagePath: 'assets/images/'
+			});
+
+		});
+      
+	</script>
+    
+</body>
+</html>
